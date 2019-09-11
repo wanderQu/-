04acd19404edf79d79bd08970bb9d28f950de204
wanderQu <42343556+wanderQu@users.noreply.github.com>
2019-09-11 17:59:49 +0800

    2019-9-11
    2019-9-11

--- js/host.js
+++ js/host.js
@@ -1,11 +1,35 @@
-function ChangeFrame(dev){
+// window.onload = function () {
+//     ClickDiv();
+// }
+
+function ChangeChildFrame(dev){
     let src = "";
-    if(dev === "log")
-        src = "../home/log.html";
-    else if(dev === "home")
+    if(dev === "home")
         src = "../home/home.html";
-    else if(dev === "admin")
-        src = "../home/admin.html";
-    document.getElementById("frame").contentWindow.location.reload();
-    document.getElementById("frame").src = src;
+    else if(dev === "normal")
+        src = "../home/normal.html";
+    else if(dev==="faqJQ")
+        src = "../home/faqJQ.html";
+    else if(dev === "system")
+        src = "../home/system.html";
+    document.getElementById("childFrame").src = src;
 }
+
+// function ClickDiv() {
+//     let home = document.getElementById("home");
+//     let normal = document.getElementById("normal");
+//     let faqJQ = document.getElementById("faqJQ");
+//     let system = document.getElementById("system");
+//     home.onclick = function () {
+//         ChangeChildFrame("home");
+//     }
+//     normal.onclick = function () {
+//         ChangeChildFrame("normal");
+//     }
+//     faqJQ.onclick =function(){
+//         ChangeChildFrame("faqJQ");
+//     }
+//     system.onclick = function () {
+//         ChangeChildFrame("system");
+//     }
+// }
